18a60ea2bd46e197131feb836827223a2ec4e769
Gregory Jefferis <jefferis@gmail.com>
2015-07-20 12:04:04 +0100

    package docs

---
+++ R/manifold-package.R
@@ -0,0 +1,10 @@
+#' Manifold dimension reduction
+#'
+#' See \code{\link{manifold_reduction}}
+#'
+#' @name manifoldreduction-package
+#' @aliases manifoldreduction
+#' @seealso \code{\link{manifold_reduction}}
+#' @docType package
+#' @keywords package
+NULL
